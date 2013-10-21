52281352d4af654a86b1f1d467860e478f0f8630
John Davidson <jdavidson091@gmail.com>
2013-10-21 02:19:09 -0500

    Fixed new user creation redirection and header layout.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the application. Enjoy!"
       redirect_to @user
     else
